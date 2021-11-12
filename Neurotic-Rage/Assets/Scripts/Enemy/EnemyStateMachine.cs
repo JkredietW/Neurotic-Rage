4eb7dfb560a97c212be44b7b8f6a8d8c08f5a06c
JonasLK <49680607+JonasLK@users.noreply.github.com>
2021-11-12 14:04:44 +0100

    NavMesh + StateMachine

--- Neurotic-Rage/Assets/Scripts/Enemy/EnemyStateMachine.cs
+++ Neurotic-Rage/Assets/Scripts/Enemy/EnemyStateMachine.cs
@@ -1,10 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyStateMachine : MonoBehaviour
 {
-    
+    public float attackRange;
+    public float chaseRange;
+
+    public GameObject player;
+    public NavMeshAgent navMeshAgent;
 
     EnemyStates currentEnemyState;
 
@@ -13,7 +18,8 @@ public class EnemyStateMachine : MonoBehaviour
         standby,
         chase,
         patrol,
-        attack
+        attack,
+        dying
     }
 
     void Start()
@@ -45,6 +51,9 @@ public class EnemyStateMachine : MonoBehaviour
                 print("4");
 
                 break;
+            case EnemyStates.dying:
+                EnemyDyingState();
+                break;
             default:
                 print("5");
                 Debug.LogError("stateChanger reached default state");
@@ -55,20 +64,31 @@ public class EnemyStateMachine : MonoBehaviour
 
     public void EnemyStandbyState()
     {
-
+        EnterChaseState();
     }
 
     public void EnemyChaseState()
     {
-
+        navMeshAgent.SetDestination(player.transform.position);
+        if(Vector3.Distance(gameObject.transform.position, player.transform.position) <= attackRange)
+        {
+            currentEnemyState = EnemyStates.attack;
+        }
     }
 
     public void EnemyPatrolState()
     {
-
+        EnterChaseState();
     }
 
     public void EnemyAttackState()
+    {
+        //attack animation and damage stuff (maybe spawn in hitbox)
+        print("Attack!");
+        currentEnemyState = EnemyStates.chase;
+    }
+
+    public void EnemyDyingState()
     {
 
     }
@@ -77,6 +97,21 @@ public class EnemyStateMachine : MonoBehaviour
 
     public void EnterChaseState()
     {
-        //if()
+        print("enter chase 1");
+        if (Vector3.Distance(gameObject.transform.position, player.transform.position) <= chaseRange)
+        {
+            print("enter chase 2");
+            Debug.DrawLine(transform.position, (player.transform.position - gameObject.transform.position));
+            RaycastHit hit;
+            if(Physics.Raycast(transform.position, (player.transform.position - gameObject.transform.position), out hit, Mathf.Infinity))
+            {
+                print("enter chase 3");
+                if (hit.transform.gameObject.layer == 12)
+                {
+                    print("Chasing");
+                    currentEnemyState = EnemyStates.chase;
+                }
+            }
+        }
     }
 }
