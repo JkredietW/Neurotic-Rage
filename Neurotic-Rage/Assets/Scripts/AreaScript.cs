a618e30afbb1c769e38a3afcba8a5f664d17e70a
ATkingma <keizerxjwz4@gmail.com>
2022-01-02 02:41:41 +0100

    area's discoverd wauw!

--- Neurotic-Rage/Assets/Scripts/AreaScript.cs
+++ Neurotic-Rage/Assets/Scripts/AreaScript.cs
@@ -4,12 +4,30 @@ using UnityEngine;
 
 public class AreaScript : MonoBehaviour
 {
+	public int areaDistrict;
+	private void Start()
+	{
+		//PlayerPrefs.SetInt("Area" + areaDistrict.ToString(), 1);
+		if (PlayerPrefs.GetInt("Area" + areaDistrict.ToString()) == 2)
+		{
+			Destroy(gameObject);
+		}
+	}
 	private void OnTriggerEnter(Collider other)
 	{
 		if (other.transform.CompareTag("Player"))
 		{
-			//PlayerPrefs.SetInt("")
-			Destroy(gameObject);
+			if (PlayerPrefs.GetInt("Area" + areaDistrict.ToString())==1)
+			{
+				PlayerPrefs.SetInt("Area" + areaDistrict.ToString(),2);
+				int discoverdAreas = PlayerPrefs.GetInt("DiscoverdArea");
+				PlayerPrefs.SetInt("DiscoverdArea", discoverdAreas + 1);
+				Destroy(gameObject);
+			}
+			else
+			{
+				Destroy(gameObject);
+			}
 		}
 	}
 }
